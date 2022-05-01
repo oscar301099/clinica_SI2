cdfa8055f0349a66ec99d8c52befe97f9c18a9c3
oscar <oscarorosduran@gmail.com>
2022-05-01 11:44:33 -0400

    asdas

--- app/Models/Binnacle.php
+++ app/Models/Binnacle.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bitacora extends Model
 {
+         
     use HasFactory;
     protected $table='bitacora'; //usa el nombre de la base de datos 
     protected $fillable = ['id','Accion','ID_User'];
